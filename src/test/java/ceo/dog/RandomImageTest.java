b353c8cde9c5e9ec22033c415993137cf71de23b
Carol Mirakove <carolkmirakove@gmail.com>
2024-02-18 17:34:43 -0500

    add TODO; comment on assertion

--- src/test/java/ceo/dog/RandomImageTest.java
+++ src/test/java/ceo/dog/RandomImageTest.java
@@ -7,23 +7,17 @@ import static org.hamcrest.Matchers.*;
 
 public class RandomImageTest {
 
+    // TODO replace IMAGE_RANDOM constant with test environment constants for base urls and resource constants for API endpoints
     public static final String IMAGE_RANDOM = "https://dog.ceo/api/breeds/image/random";
 
+    // The Dog API project only accepts jpg files per https://github.com/jigsawpieces/dog-api-images/?tab=readme-ov-file#submission-guide
     @Test(groups ={"smoke", "regression"})
-    public void testStatusCode() {
+    public void testStatusCodeAndFileType() {
         given()
                 .when()
                 .get(IMAGE_RANDOM)
                 .then()
-                .statusCode(200);
-    }
-
-    @Test(groups ="regression")
-    public void testFileType() {
-        given()
-                .when()
-                .get(IMAGE_RANDOM)
-                .then()
+                .statusCode(200)
                 .body("message", endsWith(".jpg"));
     }
 }
